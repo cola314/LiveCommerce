b0ed01b00346e16b6f0cc3df13718403938e10e6
noah <ssh7636@gmail.com>
2022-12-05 18:05:17 +0900

    add Video

--- LiveCommerce/src/main/frontend/src/App.js
+++ LiveCommerce/src/main/frontend/src/App.js
@@ -1,16 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 512px;
+  height: 368px;
+  position: relative; 
+  background: white;
+  border-radius: 16px;
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
+  margin: 0 auto; 
+  margin-top: 100px;
+  margin-bottom: 32px;
+  display: flex;
+  flex-direction: column;
+`;
+
 
 function App() {
-  const [enter, setEnter] = useState('');
+  const hostVideoRef = useRef(null);
+  function getConnectedDevices(type, callback) {
+    navigator.mediaDevices.enumerateDevices()
+        .then(devices => {
+          const filtered = devices.filter(device => device.kind === type);
+          callback(filtered);
+        });
+  }
+  getConnectedDevices('videoinput', cameras => console.log('Cameras found', cameras.length));
 
-  useEffect(() => {
-    axios.get('http://localhost:8080/api/enter')
-        .then(resp => setEnter(resp.data))
-  }, []);
+  async function playVideoFromCamera() {
+    try {
+      const constraints = { 'video': true };
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      hostVideoRef.current.srcObject = stream;
+
+    } catch (error) {
+      console.error('Error opening video camera.', error);
+    }
+  }
+
+  playVideoFromCamera();
 
   return (
-    <div>{enter}</div>
+      <>
+        <Container>
+          <video id="localVideo" ref={hostVideoRef} autoPlay />
+        </Container>
+      </>
   );
 }
 
