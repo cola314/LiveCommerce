9b4f301d6346ffb16ef610d3688d08bba81d67ab
noah <ssh7636@gmail.com>
2022-12-05 19:22:52 +0900

    websocket 연결(stomp)

--- LiveCommerce/src/main/frontend/src/App.js
+++ LiveCommerce/src/main/frontend/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, useRef } from "react";
-import axios from "axios";
+import React, {useRef, useState,  useEffect} from "react";
 import styled from "styled-components";
+import * as StompJs from '@stomp/stompjs';
+import * as SockJS from 'sockjs-client';
 
 const Container = styled.div`
   width: 512px;
@@ -16,38 +17,76 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
-
 function App() {
-  const hostVideoRef = useRef(null);
-  function getConnectedDevices(type, callback) {
-    navigator.mediaDevices.enumerateDevices()
-        .then(devices => {
-          const filtered = devices.filter(device => device.kind === type);
-          callback(filtered);
-        });
-  }
-  getConnectedDevices('videoinput', cameras => console.log('Cameras found', cameras.length));
 
-  async function playVideoFromCamera() {
-    try {
-      const constraints = { 'video': true };
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      hostVideoRef.current.srcObject = stream;
+    // 웹소켓 구현부
+    const [content, setContent] = useState("");
+    const client = new StompJs.Client({
+        brokerURL: 'ws://localhost:8080/ws', //
+        connectHeaders: {
+            login: 'user',
+            password: 'password'
+        },
+        debug: function (str) {
+            console.log(str);
+        },
+    });
 
-    } catch (error) {
-      console.error('Error opening video camera.', error);
+    client.activate();
+
+    const wsSubscribe = () => {
+        client.onConnect = () => {
+            client.subscribe('/topic/message', (msg) => {
+                console.log(msg.headers);
+            }, {id: "user"})
+        }
     }
-  }
 
-  playVideoFromCamera();
+    wsSubscribe();
 
-  return (
-      <>
-        <Container>
-          <video id="localVideo" ref={hostVideoRef} autoPlay />
-        </Container>
-      </>
-  );
+    const sendChat = () => {
+        client.publish({
+            destination: '/chat',
+            body: JSON.stringify({
+                'message': "test"
+            }),
+        })
+    }
+
+
+
+    // Video 구현부
+    const hostVideoRef = useRef(null);
+    function getConnectedDevices(type, callback) {
+        navigator.mediaDevices.enumerateDevices()
+            .then(devices => {
+              const filtered = devices.filter(device => device.kind === type);
+              callback(filtered);
+            });
+    }
+    getConnectedDevices('videoinput', cameras => console.log('Cameras found', cameras.length));
+
+    async function playVideoFromCamera() {
+        try {
+          const constraints = { 'video': true };
+          const stream = await navigator.mediaDevices.getUserMedia(constraints);
+          hostVideoRef.current.srcObject = stream;
+
+        } catch (error) {
+          console.error('Error opening video camera.', error);
+        }
+    }
+
+    playVideoFromCamera();
+
+    return (
+          <>
+            <Container>
+              <video id="localVideo" ref={hostVideoRef} autoPlay />
+            </Container>
+              <button onClick = {sendChat}>전송</button>
+          </>
+    );
 }
 
 export default App;
